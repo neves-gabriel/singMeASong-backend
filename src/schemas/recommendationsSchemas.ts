a966a60aba3333e6349267ed29a97b5ee0e576b8
neves-gabriel <gabrielrn.mobres@gmail.com>
2022-05-09 17:27:15 -0300

    fix: :bug: fixed bug in regex

--- src/schemas/recommendationsSchemas.ts
+++ src/schemas/recommendationsSchemas.ts
@@ -2,7 +2,7 @@ import joi from "joi";
 
 import { CreateRecommendationData } from "../services/recommendationsService.js";
 
-const youtubeLinkRegex = /^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/;
+const youtubeLinkRegex = /^(https?:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/;
 
 export const recommendationSchema = joi.object<CreateRecommendationData>({
   name: joi.string().required(),
